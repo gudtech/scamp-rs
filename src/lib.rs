04348fec6956d4c8c5ab6f60ac2e91158aef7517
Daniel Norman <daniel@danielnorman.net>
2019-04-09 10:50:45 -0700

    Compiles now

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,13 @@
 
 #![allow(dead_code)]
 #![allow(unused_imports)]
+#![allow(unused_variables)]
 
-// The nightly features that are commonly needed with async / await
+// enable the await! macro, async support, and the new std::Futures api.
 #![feature(await_macro, async_await, futures_api)]
-//
-//// enable the await! macro, async support, and the new std::Futures api.
-//#![feature(await_macro, async_await, futures_api)]
-//
-//// only needed if we want to manually write a method to go forward from 0.1 to 0.3 future,
-//// or manually implement a std future (it provides Pin and Unpin):
-//#![feature(pin)]
-//// only needed to manually implement a std future:
-//#![feature(arbitrary_self_types)]
+
+// only needed to manually implement a std future:
+#![feature(arbitrary_self_types)]
 
 
 // This pulls in the `tokio-async-await` crate. While Rust 2018 doesn't require
